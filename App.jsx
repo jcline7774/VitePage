90d4ed3e062ca218073ce8604a584f56549ad7af
jcline7774 <john7774@icloud.com>
2024-11-15 18:15:55 -0500

    add: deploy workflow

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Hero from './components/hero/Hero'
+import Pages from './components/pages/pages'
+
+
+function App() {
+
+
+  return (
+    <>
+     <Hero />
+     <Pages />
+    </>
+  )
+}
+
+export default App
---
+++ components/pages/pages.jsx
@@ -0,0 +1,41 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './pages.css'
+
+const pages = () => {
+  return (
+    <section className='home'>
+        <div className='home-content'>
+
+            <h1> WELCOME TO THE AUTO REVOLUTION</h1>
+
+            <h3>Globally Dominating Vehicle Brands</h3>
+
+            <p><b>New and Future Concept Cars that can be viewed online or at our dealership.</b></p>
+
+            <a className="btn">Industry Leaders</a> 
+             
+
+        </div>
+
+        <div className="home-img">
+
+            <div className="rhombus">
+
+                <img src="car.png" />
+
+            </div>
+
+            </div>
+
+            <div className="rhombus2">
+
+            </div>
+
+
+        
+    </section>
+  )
+}
+
+export default pages
