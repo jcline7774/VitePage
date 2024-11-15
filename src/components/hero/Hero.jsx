9f690b6ac32fb0ee73928e5561ff1c37c90ae52e
jcline7774 <john7774@icloud.com>
2024-11-15 18:48:11 -0500

    add: deploy workflow

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+    return (
+        <div className="header">
+
+                    <a href="#" class="logo">NewCars.Autohaus</a>
+
+                    <div className="navbar">
+
+                        <a href="">Buy</a>
+                        <a href="">How it works</a>
+                        <a href="">Concepts</a>
+                        <a href="">Services</a>
+                        <a href="">Awards</a>
+                    
+                    
+                    
+                    </div>
+
+                    <div className="social-media">
+                        
+                        <a href="#" ><img src="fb.png" /></a>
+                        <a href="#" ><img src="ins.png" /></a>
+                        <a href="#" ><img src="twitter.png" /></a>
+
+                        </div>
+         
+        
+        
+        </div>
+    )
+}   
+
+export default Hero 
